fe4cf23b4003998f1cf824e9b38bc7002e847e25
skurzyp-arianelabs <stanislaw.kurzyp@arianelabs.com>
2025-08-26 11:12:56 +0200

    feat: add token info query action (#174)
    Signed-off-by: skurzyp-arianelabs <stanislaw.kurzyp@arianelabs.com>

--- typescript/src/shared/hedera-utils/mirrornode/hedera-mirrornode-service.interface.ts
+++ typescript/src/shared/hedera-utils/mirrornode/hedera-mirrornode-service.interface.ts
@@ -3,7 +3,7 @@ import {
   AccountResponse,
   TokenBalancesResponse,
   TopicMessagesResponse,
-  TokenDetails,
+  TokenInfo,
 } from './types';
 
 export interface IHederaMirrornodeService {
@@ -11,5 +11,5 @@ export interface IHederaMirrornodeService {
   getAccountHBarBalance(accountId: string): Promise<BigNumber>;
   getAccountTokenBalances(accountId: string): Promise<TokenBalancesResponse>;
   getTopicMessages(queryParams: TopicMessagesQueryParams): Promise<TopicMessagesResponse>;
-  getTokenDetails(tokenId: string): Promise<TokenDetails>;
+  getTokenInfo(tokenId: string): Promise<TokenInfo>;
 }
